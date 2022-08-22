a1cecbcaa5ac9ff2d41e7450a837af13a3161d3b
AlexBlok1083 <alexblok50@gmail.com>
2022-08-22 15:12:47 +0200

    Ball en walls
    De bal is teogevoegd en de walls waar de bal van af bounced zijn toegevoegd.

---
+++ Pong/Assets/Scripts/Ball.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb2d;
+    [SerializeField] private float maxInitialAngle = 0.67f;
+    [SerializeField] private float moveSpeed = 1f;
+    private void Start()
+    {
+        InitialPush();
+    }
+
+    private void InitialPush() {
+        Vector2 dir = Vector2.left;
+        dir.y = Random.Range(-maxInitialAngle, maxInitialAngle);
+        rb2d.velocity = dir * moveSpeed;
+    }
+
+    
+}
