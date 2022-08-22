298419543fe18acbd024ccd084c239a2b73068e9
AlexBlok1083 <alexblok50@gmail.com>
2022-08-22 16:49:54 +0200

    Reset Ball
    De bal word gereset na dat hij in 1 van de scorezones is gekomen.

--- Pong/Assets/Scripts/Ball.cs
+++ Pong/Assets/Scripts/Ball.cs
@@ -7,22 +7,34 @@ public class Ball : MonoBehaviour
     [SerializeField] private Rigidbody2D rb2d;
     [SerializeField] private float maxInitialAngle = 0.67f;
     [SerializeField] private float moveSpeed = 1f;
+    [SerializeField] private float maxStartY = 4f;
+
+    private float startX = 0f;
+
     private void Start()
     {
         InitialPush();
     }
 
     private void InitialPush() {
-        Vector2 dir = Vector2.left;
+        Vector2 dir = Random.value < 0.5f ? Vector2.left : Vector2.right;
         dir.y = Random.Range(-maxInitialAngle, maxInitialAngle);
         rb2d.velocity = dir * moveSpeed;
+    }
+
+    private void Resetball()
+    {
+        float posY = Random.Range(-maxStartY, maxStartY);
+        Vector2 position = new Vector2(startX, posY);
+        transform.position = position;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         ScoreZone scoreZone = collision.GetComponent<ScoreZone>();
-        if (scoreZone) { 
-        
+        if (scoreZone) {
+            Resetball();
+            InitialPush();
         }
     }
 }
